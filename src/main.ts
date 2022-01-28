fe5288daf4f8acb5f8024452ed1d4622e5fd6784
ryuga0719 <ryuga.private@gmail.com>
2022-01-28 23:15:34 +0900

    basic program created

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
-const app = document.querySelector<HTMLDivElement>('#app')!
+// containerElement
+const container = document.querySelector<HTMLElement>('.container');
+// childElement
+const child = document.querySelector<HTMLElement>('.child');
+// childの図形の直径
+const diameter = 50;
+// childの図形の半径
+const radius = diameter / 2;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+/**
+ * mouseMove
+ */
+const mouseMove = (e: MouseEvent) => {
+  // child!.style.left = e.clientX - radius + "px";
+  // child!.style.top = e.clientY - radius + "px";
+  child!.style.transform = 'translate(' + (e.clientX - radius) + 'px, ' + (e.clientY - radius) + 'px)';
+}
+
+child!.style.width = diameter + "px";
+child!.style.height = diameter + "px";
+container!.addEventListener("mousemove", mouseMove);
+
+
