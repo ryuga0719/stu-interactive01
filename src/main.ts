f1becc98ada62d5a4e0620496fd231823fbdbdb4
ryuga0719 <ryuga.private@gmail.com>
2022-02-11 11:58:25 +0900

    基本機能のモジュール化

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,10 @@
-// containerElement
-const container = document.querySelector<HTMLElement>('.container');
-// childElement
-const child = document.querySelector<HTMLElement>('.child');
-// childの図形の直径
-const diameter = 50;
-// childの図形の半径
-const radius = diameter / 2;
+import Basic from './modules/Basic';
 
-/**
- * mouseMove
- */
-const mouseMove = (e: MouseEvent) => {
-  // child!.style.left = e.clientX - radius + "px";
-  // child!.style.top = e.clientY - radius + "px";
-  child!.style.transform = 'translate(' + (e.clientX - radius) + 'px, ' + (e.clientY - radius) + 'px)';
+class Test extends Basic {
+  constructor() {
+    super();
+  }
+
 }
 
-child!.style.width = diameter + "px";
-child!.style.height = diameter + "px";
-container!.addEventListener("mousemove", mouseMove);
-
-
+const test = new Test();
