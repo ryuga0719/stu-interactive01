bfcf2a5e982cf7b29aff7cfdc38131e6a69b37ca
ryuga0719 <ryuga.private@gmail.com>
2022-02-24 09:43:29 +0900

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 module.exports = {
+  lang: "ja-JP",
   base: "./",
   assetsDir: "./",
+  server: {
+    host: true,
+    port: 8888, // 任意のポート番号を書く
+  },
   build: {
+    manifest: true,
+    publicDir: "public",
     outDir: "docs",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: "assets/js/[name].js",
+        entryFileNames: "assets/js/[name].js",
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false,
+      },
+    },
   },
 };
